9076ff900aac60f4ee32a2f03b4373ac0df04d9a
Sanjeev jaiswal <jaiswalsanjeev446@gmail.com>
2025-11-22 10:23:32 +0530

    Implement touch movement for mobile devices
    Added touch event listener for mobile cursor movement.

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var main = document.querySelector("main");
 var crsr = document.querySelector(".cursor");
 
+// Mouse movement (Laptop/Desktop)
 main.addEventListener("mousemove", (dets) => {
-  //   console.log(dets.x, dets.y);
   crsr.style.left = dets.x + "px";
   crsr.style.top = dets.y + "px";
+});
+
+// Touch movement (Mobile)
+main.addEventListener("touchmove", (e) => {
+  let touch = e.touches[0];
+  crsr.style.left = touch.clientX + "px";
+  crsr.style.top = touch.clientY + "px";
 });
